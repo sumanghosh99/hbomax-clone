05c70972d8374ad069884b6790570bed161ab007
sumit kumar <93507024+sumit7195@users.noreply.github.com>
2022-02-25 20:05:09 +0530

    complete signUp page
    completed whole signup page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { SignUp } from './SignUp'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+
+
+    <SignUp></SignUp>
+
+
+    </div>
+  )
+}
+
+export default App
